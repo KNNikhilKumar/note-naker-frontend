33d619f5df113bb81dbf0d1be50b81980818e303
nikhil <nikhilnaveenkn@gmail.com>
2023-07-03 18:23:37 +0530

    note_maker | changes related to editing done

--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -1,17 +1,46 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useLocation, useNavigate } from 'react-router-dom';
 const EditNote = () => {
+  const location=useLocation();
+  const navigate=useNavigate();
+
+  const [noteTitle,noteTitleChange]=useState(location.state.title);
+  const [noteContent,noteContentChange]=useState(location.state.content)
+
+
+  const modifyNote=async ()=>{
+
+      const resp=await fetch('http://localhost:8000/edit',{
+        method:"PUT",
+        headers:{
+          "email":location.state.email,
+          "_id":location.state._id,
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          title:noteTitle,
+          content:noteContent
+        })
+      })
+
+      console.log(resp.json());
+
+      navigate('/');
+  }
+
   return (
     <Form>
     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
       <Form.Label>Title</Form.Label>
-      <Form.Control type="text" placeholder="EX:Solar System" />
+      <Form.Control type="text" placeholder="EX:Solar System" value={noteTitle} onChange={(e)=>noteTitleChange(e.target.value)} />
     </Form.Group>
     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
       <Form.Label>Example textarea</Form.Label>
-      <Form.Control as="textarea" rows={10} />
+      <Form.Control as="textarea" rows={10} value={noteContent} onChange={(e)=>noteContentChange(e.target.value)}/>
     </Form.Group>
-    <Button variant="primary" type="submit">
+    <Button variant="primary" onClick={modifyNote}>
         Submit
     </Button>
   </Form>
