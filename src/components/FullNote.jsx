5487265050b9980e724fdfe2a3129f7b0873ce6e
nikhil <nikhilnaveenkn@gmail.com>
2023-07-03 17:37:16 +0530

    note-maker | delete feature added

--- src/components/FullNote.jsx
+++ src/components/FullNote.jsx
@@ -1,7 +1,25 @@
 import { useLocation } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const FullNote = () => {
 const location=useLocation();
+const navigate=useNavigate();
+
+  const onClickEditButton=()=>{
+      navigate('/edit',{state:location.state});
+  }
+
+  const onClickDeleteButton=async ()=>{
+      const resp=await fetch('http://localhost:8000/delete',{
+        method:'DELETE',
+        headers:{
+          'email':location.state.email,
+          '_id':location.state._id
+        }
+      });
+      console.log(resp.json());
+      navigate("/");
+  }
   return (
     <div>
         <br />
@@ -12,8 +30,8 @@ const location=useLocation();
         </p>
         <table>
           <tr>
-            <td><button type="button" class="btn btn-secondary" name="modify">Modify</button></td>
-            <td> <button type="submit" class="btn btn-danger" name="delete">Delete</button></td>
+            <td><button type="button" class="btn btn-secondary" name="modify" onClick={onClickEditButton}>Modify</button></td>
+            <td> <button type="submit" class="btn btn-danger" name="delete" onClick={onClickDeleteButton}>Delete</button></td>
           </tr>
         </table>
     </div>
